3a28183e05c0fc8aa8b21c0e06e6c9d6c0e0eff7
Ayush Singh <singh.ayush.as@icloud.com>
2024-11-15 14:54:22 +0100

    Modify Settings View

---
+++ Demo/Views/HelpButton.swift
@@ -0,0 +1,18 @@
+//
+//  HelpButton.swift
+//  Demo
+//
+//  Created by Ayush Singh on 11/15/24.
+//
+
+import SwiftUI
+
+struct HelpButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HelpButton()
+}
