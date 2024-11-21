4c61ad3f1302347f680e4619609f58db472ea70c
Ayush Singh <singh.ayush.as@icloud.com>
2024-11-21 11:56:44 +0100

    YOLO

---
+++ Demo/Views/AddToPackButton.swift
@@ -0,0 +1,18 @@
+//
+//  AddToPackButton.swift
+//  Demo
+//
+//  Created by Ayush Singh on 11/21/24.
+//
+
+import SwiftUI
+
+struct AddToPackButton: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddToPackButton()
+}
